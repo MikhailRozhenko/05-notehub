29e0355cb5f13b1755e8a67e8d650e74742205d5
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-04-11 17:20:00 +0300

    initial project

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import 'modern-normalize/modern-normalize.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ReactDOM from 'react-dom/client';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './components/App/App';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+);
